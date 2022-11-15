185afa0a4543ebcc5d62c98c586d50e8fe984ffd
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2022-11-15 16:29:45 +0000

    Displaying data from both APIs

--- RodWeatherApp/script.js
+++ RodWeatherApp/script.js
@@ -1,12 +1,13 @@
 const search = document.getElementById("search");
 const userInput = document.getElementById("userInput");
-const backgroundImg = document.getElementsByTagName("body")
+const backgroundImg = document.getElementById("backPic")
 const cityWeather = document.getElementById("cityWeather")
 const tempDisp = document.getElementById("tempDisp")
 const iconDisp = document.getElementById("iconDisp")
 const descriptionDisp = document.getElementById("descriptionDisp")
 const humidDisp = document.getElementById("humidDisp")
 const windDisp = document.getElementById("windDisp")
+const feelLike = document.getElementById("feelLike")
 
 let apiKey = config.apiWeatherKey;
 let clientID = config.clientUnsplash;
@@ -26,7 +27,7 @@ userInput.addEventListener('keydown', function (event) {
 })
 
 let requestWeather = () => {
-
+    
     let city = String(userInput.value)
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
@@ -46,14 +47,17 @@ let requestWeather = () => {
     let displayWeather = (data) => {
         const { name } = data
         const { icon, description } = data.weather[0]
-        const { temp, humidity } = data.main
+        const { temp, humidity, feels_like } = data.main
         const { speed } = data.wind
+        const { country } = data.sys
 
         const tempInt = Math.floor(temp)
+        const feelLikeInt = Math.floor(feels_like)
 
-        cityWeather.textContent = `Weather in ${name}`
+        cityWeather.textContent = `Weather in ${name}, ${country}`
         tempDisp.textContent = `${tempInt}° C`
         descriptionDisp.textContent = `${description}`
+        feelLike.textContent = `Feels like: ${feelLikeInt}° C`
         humidDisp.textContent = `Humidity: ${humidity}%`
         windDisp.textContent = `Wind speed: ${speed} km/h`
         iconDisp.src = `http://openweathermap.org/img/wn/${icon}.png`
@@ -64,7 +68,7 @@ let requestImg = () => {
 
     let userSearch = String(userInput.value)
 
-    let urlImg = `https://api.unsplash.com/search/photos?client_id=${clientID}&query=${userSearch}`
+    let urlImg = `https://api.unsplash.com/search/photos?client_id=${clientID}&query=${userSearch}&orientation=landscape`
 
     fetch(urlImg)
 
@@ -73,9 +77,14 @@ let requestImg = () => {
         })
 
         .then(data => {
-            console.log(data)
+            displayImg(data)
         })
-    
-        // backgroundImg.style.backgroundImage = `url(${data.results[0].urls.raw})`;
 
+    let randNumber = Math.floor(Math.random() * 10);
+
+    let displayImg = (data) => {
+        const { raw } = data.results[randNumber].urls
+
+        backgroundImg.src = `${raw}`;
+    }
 }
